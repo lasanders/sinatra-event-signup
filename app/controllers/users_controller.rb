846b893f143b9ff0e54b78de51da52b671f07939
Lara Sanders <larasanders@gmail.com>
2018-01-23 20:00:16 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/signup' do
+   if !session[:user_id]
+     erb :'users/signup'
+   else
+
+     redirect '/tweets'
+ end
+end
+
+  post '/signup' do
+    if params[:username].empty? || params[:password].empty? || params[:email].empty?
+      redirect '/signup'
+
+    else
+   @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+   @user.save
+   session[:user_id] = @user.id
+
+   redirect '/tweets'
+ end
+end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.clear
+      redirect to '/login'
+  else
+  redirect to '/'
+   end
+end
+
+  end
